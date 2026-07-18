28da3f10a74c66b6b6f3154a792e72dc2e316385
Abhay12git <abhaykowshik@gmail.com>
2026-07-18 23:24:39 +0530

    fix(frontend): exclude shared package from vite optimizeDeps to fix CJS getter crash

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,3 +1,5 @@
 export type { Identifier, Char, CRDTOperation } from './crdt/types';
-export { FractionalIndex } from './crdt/FractionalIndex';
-export { SequenceCRDT } from './crdt/SequenceCRDT';
+import { FractionalIndex } from './crdt/FractionalIndex';
+import { SequenceCRDT } from './crdt/SequenceCRDT';
+
+export { FractionalIndex, SequenceCRDT };
